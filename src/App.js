febb23a9d3ca4fe4bb9dc09390e6811eaecf745d
Dmitriy Dihtiarov <dmitriydegtyarev1985@gmail.com>
2021-06-27 14:38:29 +0300

    finish lesson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import PaintingList from './components/PaintingList';
+import Section from './components/Section';
+import paintings from './paintings.json';
 
-function App() {
+export default function App() {
+  // const isOnline = false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Section title="Топ недели">
+        <PaintingList items={paintings} />
+      </Section>
+      <Section />
+      {/* <PaintingList items={paintings} /> */}
+      {/* {isOnline && 'Онлайн'}
+      {isOnline ? 'Онлайн' : 'Офлайн'} */}
+      {/* <Painting
+        imageUrl={paintings[0].url}
+        title={paintings[0].title}
+        author={paintings[0].author.tag}
+        profileUrl={paintings[0].author.url}
+        price={paintings[0].price}
+        quantity={paintings[0].quantity}
+      />
+      <Painting
+        imageUrl={paintings[1].url}
+        title={paintings[1].title}
+        author={paintings[1].author.tag}
+        profileUrl={paintings[1].author.url}
+        price={paintings[1].price}
+        quantity={paintings[1].quantity}
+      />
+      <Painting
+        imageUrl={paintings[2].url}
+        title={paintings[2].title}
+        author={paintings[2].author.tag}
+        profileUrl={paintings[2].author.url}
+        price={paintings[2].price}
+        quantity={paintings[2].quantity}
+      /> */}
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,97 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+// import paintings from './paintings.json';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const elem1 = React.createElement('span', { children: 'Привет' });
+// const elem1 = <span>Привет</span>;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const elem2 = React.createElement('span', { children: 'мир' });
+// const elem2 = <span>Мир</span>;
+
+// const element = React.createElement('div', { a: 5, b: 10, children: 'Привет мир' });
+
+// const jsxElement = (
+//   <div>
+//     {elem1}
+//     {elem2}
+//   </div>
+// );
+
+// console.log(jsxElement);
+
+// const data = {
+//   id: 'id-1',
+//   url: 'https://cdn.pixabay.com/photo/2017/07/31/22/05/feathers-2561511_1280.jpg',
+//   title: 'Feathers. Art abstract',
+//   price: 500,
+//   author: {
+//     tag: 'ractapopulous',
+//     url: 'https://pixabay.com/users/ractapopulous-24766/',
+//   },
+//   quantity: 10,
+// };
+
+// const data = paintings[0];
+
+// function Painting(props) {
+//   const { url, title, profileUrl, author, price } = props;
+//   return (
+//     <div>
+//       <img src={url} alt={title} width="480" />
+//       <h2>{title}</h2>
+//       <p>
+//         Автор: <a href={profileUrl}>{author}</a>
+//       </p>
+//       <p>Цена: {price} кредитов</p>
+//       <p>Доступность: заканчивается или есть в наличии</p>
+//       <button type="button">Добавить в корзину</button>
+//     </div>
+//   );
+// }
+
+// const painting = (
+//   <div>
+//     <img src={data.url} alt={data.title} width="480" />
+//     <h2>{data.title}</h2>
+//     <p>
+//       Автор: <a href={data.author.url}>{data.author.tag}</a>
+//     </p>
+//     <p>Цена: {data.price} кредитов</p>
+//     <p>Доступность: заканчивается или есть в наличии</p>
+//     <button type="button">Добавить в корзину</button>
+//   </div>
+// );
+
+// const painting = paintings[1];
+
+// url={painting.url}
+//     title={painting.title}
+//     author={painting.author.tag}
+//     profileUrl={painting.author.url}
+//     price={painting.price}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+// ReactDOM.render(painting, document.querySelector('#root'));
+
+// ReactDOM.render(element, document.querySelector('#root'));
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
