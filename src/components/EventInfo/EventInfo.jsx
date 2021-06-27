44002284d7602655796190f1797d72f2c8580810
Dmitriy Dihtiarov <dmitriydegtyarev1985@gmail.com>
2021-06-28 00:57:37 +0300

    finish lesson

---
+++ src/components/EventInfo/EventInfo.jsx
@@ -0,0 +1,10 @@
+import css from './EventInfo.module.css';
+
+export const EventInfo = ({ text, children }) => {
+  return (
+    <p className={css.info}>
+      {children}
+      {text}
+    </p>
+  );
+};
---
+++ src/utils/formatEventDuration.js
@@ -0,0 +1,6 @@
+import { formatDistance } from 'date-fns';
+import { uk } from 'date-fns/locale';
+
+export const formatEventDuration = (start, end) => {
+  return formatDistance(Date.parse(start), Date.parse(end), { locale: uk });
+};
